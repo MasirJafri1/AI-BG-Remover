41e314e0486c080e034b469cb19e351ab63a48a7
MasirJafri1 <masirjafri1@gmail.com>
2025-01-08 16:10:37 +0530

    Backend Done for credits Purchasing

--- Server/Controllers/User.controller.js
+++ Server/Controllers/User.controller.js
@@ -1,5 +1,7 @@
 import { Webhook } from "svix";
 import userModel from "../Models/User.model.js";
+import razorpay from "razorpay";
+import transactionModel from "../Models/Transaction.model.js";
 
 // API controller function to manage clerk user with DB
 
@@ -73,4 +75,125 @@ const userCredits = async (req, res) => {
   }
 };
 
-export { clerkWebhooks, userCredits };
+// Payment gateway initialization
+const razorPayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+// API to make payment for credits
+
+const paymentRazorPay = async (req, res) => {
+  try {
+    const { clerkId, planId } = req.body;
+
+    const userData = await userModel.findOne({ clerkId });
+
+    if (!userData || !planId) {
+      return res.json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    let credits, plan, amount, date;
+
+    switch (planId) {
+      case "Basic":
+        plan = "Basic";
+        credits = 5;
+        amount = 10;
+        break;
+
+      case "Advanced":
+        plan = "Advanced";
+        credits = 20;
+        amount = 50;
+        break;
+
+      case "Business":
+        plan = "Business";
+        credits = 90;
+        amount = 200;
+        break;
+
+      default:
+        break;
+    }
+
+    date = Date.now();
+
+    // Creating transaction
+
+    const transactionData = {
+      clerkId,
+      amount,
+      credits,
+      plan,
+      date,
+    };
+
+    const newTransaction = await transactionModel.create(transactionData);
+
+    const options = {
+      amount: amount * 100,
+      currency: process.env.CURRENCY,
+      receipt: newTransaction._id,
+    };
+
+    await razorPayInstance.orders.create(options, (error, order) => {
+      if (error) {
+        return res.json({ success: false, message: "Error Occured" });
+      }
+      res.json({ success: true, order });
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API Controller to verify the razorpay Payment
+
+const verifyRazorpay = async (req, res) => {
+  try {
+    const { razorpay_order_id } = req.body;
+
+    const orderInfo = await razorPayInstance.orders.fetch(razorpay_order_id);
+
+    if (orderInfo.status === "paid") {
+      const transactionData = await transactionModel.findById(
+        orderInfo.receipt
+      );
+
+      if (transactionData.payment) {
+        return res.json({ success: false, message: "Payment Failed" });
+      }
+
+      // Adding credits in userData
+      const userData = await userModel.findOne({
+        clerkId: transactionData.clerkId,
+      });
+
+      const creditBalance = userData.creditBalance + transactionData.credis;
+
+      await userModel.findOneAndUpdate(userData._id, { creditBalance });
+
+      // Making the payment true
+
+      await transactionModel.findOneAndUpdate(transactionData._id, {
+        payment: true,
+      });
+
+      res.json({
+        success: true,
+        message: "Credits addeed",
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { clerkWebhooks, userCredits, paymentRazorPay, verifyRazorpay };
--- Server/Routes/User.route.js
+++ Server/Routes/User.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { clerkWebhooks, userCredits } from "../Controllers/User.controller.js";
+import {
+  clerkWebhooks,
+  paymentRazorPay,
+  userCredits,
+  verifyRazorpay,
+} from "../Controllers/User.controller.js";
 import AuthUser from "../MiddleWares/Auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebhooks);
 userRouter.get("/credits", AuthUser, userCredits);
+userRouter.post("/pay-razor", AuthUser, paymentRazorPay);
+userRouter.post("/verify-razor", verifyRazorpay);
 
 export default userRouter;
